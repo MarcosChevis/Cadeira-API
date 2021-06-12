772f27b45756e0b4e7ac2e3021108d4e20ae145f
Marcos Chevis Meira <marcoschevism@gmail.com>
2021-06-11 23:45:00 -0300

    html update and get from chairController now is a timer

--- src/app.js
+++ src/app.js
@@ -7,11 +7,13 @@ const router = express.Router();
 //Rotas
 const index = require('./routes/index');
 const chairRoute = require('./routes/chairRoute');
+const chairPageRoute = require('./routes/chairPageRoute');
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
 app.use('/', index);
 app.use('/chair', chairRoute);
+app.use('/chairPage', chairPageRoute)
 
 module.exports = app;
