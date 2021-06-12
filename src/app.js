9007f0c7c76f7d7184fe3a02a30b61a1c6fcc4d0
Marcos Chevis Meira <marcoschevism@gmail.com>
2021-06-12 18:27:12 -0300

    static try3

--- src/app.js
+++ src/app.js
@@ -12,12 +12,11 @@ const router = express.Router();
 //Rotas
 const index = require('./routes/index');
 const chairReleaseRoute = require('./routes/chairReleaseRoute');
-const static = require('src/public');
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
-app.use('/static', express.static(static))
+app.use('/static', express.static('src/public'))
 app.use('/', index);
 app.use('/chairRelease', chairReleaseRoute);
 
